7c605c8365d0449b296d1a50427000e093e9c69e
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2024-12-17 14:47:47 +0100

    i'm awaaaaaaaaaare

---
+++ src/Classroom.java
@@ -0,0 +1,10 @@
+public class Classroom {
+     public static void main(String[] args) {
+        Wilder Irwin = new Wilder("Irwin", true);
+        Wilder Manu = new Wilder("Manu", false);
+
+
+         System.out.println(Irwin.whoAmI());
+         System.out.println(Manu.whoAmI());
+    }
+}
